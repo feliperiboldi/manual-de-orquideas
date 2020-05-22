d09c7c11a9367bd849e23a691ea9368f541e1c4a
Felipe Riboldi <feliperiboldimarques@gmail.com>
2020-05-22 19:05:10 -0300

    Provas Sociais

---
+++ public/js/owl-scripts.js
@@ -0,0 +1,16 @@
+$('.owl-depositions').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        },
+        992:{
+            items:4
+        }
+    }
+});
